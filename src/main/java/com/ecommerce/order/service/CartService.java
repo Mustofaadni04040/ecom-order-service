e646e12727ae208c886d62286b621a4ad3a16d0c
mustofa adny <adniadni04@gmail.com>
2026-04-29 13:08:57 +0700

    circuit breaking with resilience4j

--- src/main/java/com/ecommerce/order/service/CartService.java
+++ src/main/java/com/ecommerce/order/service/CartService.java
@@ -8,6 +8,7 @@ import com.ecommerce.order.dto.UserResponse;
 import com.ecommerce.order.model.AddToCartResult;
 import com.ecommerce.order.model.CartItem;
 import com.ecommerce.order.repository.CartItemRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,7 @@ public class CartService {
     private final ProductServiceClient productServiceClient;
     private final UserServiceClient userServiceClient;
 
+    @CircuitBreaker(name = "productService")
     public AddToCartResult addToCart(String userId, CartItemRequest request) {
         ProductResponse productResponse = productServiceClient.getProductDetails(request.getProductId());
         var userResponse = userServiceClient.getUserDetails(userId);
