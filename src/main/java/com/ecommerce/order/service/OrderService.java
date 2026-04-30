037c4797738b3a19f6e8e06b71ef5bd4b557321f
mustofa adny <adniadni04@gmail.com>
2026-04-30 10:16:15 +0700

    publishing messages to rabbit mq

--- src/main/java/com/ecommerce/order/service/OrderService.java
+++ src/main/java/com/ecommerce/order/service/OrderService.java
@@ -9,10 +9,13 @@ import com.ecommerce.order.model.Order;
 import com.ecommerce.order.model.OrderItem;
 import com.ecommerce.order.model.OrderStatus;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -20,6 +23,12 @@ import java.util.Optional;
 public class OrderService {
     private final CartService cartService;
     private final OrderRepository orderRepository;
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
 
     public Optional<OrderResponse> createOrder(String userId) {
         List<CartItem> cartItems = cartService.getCart(userId);
@@ -50,6 +59,10 @@ public class OrderService {
         Order savedOrder = orderRepository.save(order);
 
         cartService.clearCart(userId);
+
+        rabbitTemplate.convertAndSend(exchangeName, routingKey,
+                Map.of("orderId", savedOrder.getId(), "status", "CREATED")
+                );
 
         return Optional.of(mapToOrderResponse(savedOrder));
 
